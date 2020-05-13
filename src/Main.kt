44af0932b6f0d2f67392483e106b69bbbb32d4f5
BuildTools <unconfigured@null.spigotmc.org>
2020-05-13 11:52:31 +0900

    Second Commit # BUG FIXED about うつ & うす

--- src/Main.kt
+++ src/Main.kt
@@ -38,38 +38,38 @@ fun main(args: Array<String>) {
     }
 
     var cur = 0
-
     val heap = Array<UByte>(512) { 0u }
     var ptr = 0
     var mode = Mode.NORMAL
 
     fun getToken(i: Int) = src[i*2].toString() + src[i*2+1]
 
-    while(cur*2+1 < src.length) {
+    a@while(cur*2+1 < src.length) {
 
-        if(mode == Mode.JUMP_F) {
-            while(getToken(cur) == "うす") ++cur
-            ++cur
-            mode = Mode.NORMAL
-            continue
-        }
-
-        if(mode == Mode.JUMP_B) {
-            while(getToken(cur) == "うつ") --cur
-            ++cur
-            mode = Mode.NORMAL
-            continue
-        }
-
-        when(getToken(cur)) {
-            "つつ" -> ++ptr
-            "つす" -> --ptr
-            "つう" -> heap[ptr]++
-            "すつ" -> heap[ptr]--
-            "すす" -> print(heap[ptr].toByte().toChar())
-            "すう" -> heap[ptr] = readLine()?.get(0)?.toByte()?.toUByte() ?: 0u
-            "うつ" -> if(heap[ptr] == 0u.toUByte()) mode = Mode.JUMP_F
-            "うす" -> mode = Mode.JUMP_B
+        when (mode) {
+            Mode.JUMP_F -> {
+                while(getToken(cur) != "うす") ++cur
+                ++cur
+                mode = Mode.NORMAL
+                continue@a
+            }
+            Mode.JUMP_B -> {
+                while(getToken(cur) != "うつ") --cur
+                ++cur
+                mode = Mode.NORMAL
+                continue@a
+            }
+            else -> when(getToken(cur)) {
+                "つつ" -> ++ptr
+                "つす" -> --ptr
+                "つう" -> heap[ptr]++
+                "すつ" -> heap[ptr]--
+                "すす" -> print(heap[ptr].toByte().toChar())
+                "すう" -> heap[ptr] = readLine()?.get(0)?.toByte()?.toUByte() ?: 0u
+                "うつ" -> if(heap[ptr] == 0u.toUByte()) { mode = Mode.JUMP_F; continue@a }
+                "うす" -> { mode = Mode.JUMP_B; continue@a }
+                "うう" -> print(heap[ptr].toByte())
+            }
         }
 
         ++cur
