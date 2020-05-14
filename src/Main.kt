019593cad2c1a15d9ff37cebbc779d57efa702db
BuildTools <unconfigured@null.spigotmc.org>
2020-05-15 08:38:23 +0900

    5th Commit # BUG FIXED

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,8 @@
 @file:Suppress("EXPERIMENTAL_UNSIGNED_LITERALS")
 
-import java.lang.Exception
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.Exception
 
 enum class Mode {
     NORMAL,
@@ -26,10 +26,10 @@ fun main(args: Array<String>) {
         args.getOrNull(0) == "-file" -> {
             String(Files.readAllBytes(Paths.get(args[1])))
                 .replace("(//.*$)".toRegex(RegexOption.MULTILINE), "")
-                .replace("\n|\r|\\s|　".toRegex(), "")
+                .replace("\n|\r|\\s|　|\t".toRegex(), "")
         }
         args.getOrNull(0) == "-con" -> {
-            println(convert(readLine()!!))
+            println(convert(String(Files.readAllBytes(Paths.get(args[1])))))
             throw Exception()
         }
         else -> {
@@ -47,36 +47,72 @@ fun main(args: Array<String>) {
 
     fun getToken(i: Int) = src[i*2].toString() + src[i*2+1]
 
-    a@while(cur*2+1 < src.length) {
+    try { a@while(cur*2+1 < src.length) {
 
         when (mode) {
             Mode.JUMP_F -> {
-                while(getToken(cur) != "うす") ++cur
+               var d = 0
+                    while(true) {
+
+                        if(getToken(cur) == "うつ")
+                            d++
+                        else if(getToken(cur) == "うす")
+                            d--
+
+                        if(getToken(cur) == "うす" && d == 0)
+                            break
+
+                    ++cur
+
+                }
                 ++cur
                 mode = Mode.NORMAL
                 continue@a
             }
             Mode.JUMP_B -> {
-                while(getToken(cur) != "うつ") --cur
+                var d = 0
+                while(true) {
+
+                    if(getToken(cur) == "うす")
+                        d++
+                    else if(getToken(cur) == "うつ")
+                        d--
+
+                    if(getToken(cur) == "うつ" && d == 0)
+                        break
+
+                    --cur
+
+                }
+                ++cur
                 mode = Mode.NORMAL
                 continue@a
             }
             else -> when(getToken(cur)) {
-                "つつ" -> ++ptr
-                "つす" -> --ptr
+                "つつ" -> {
+                    ++ptr
+//                    println("$ptr P:$cur")
+                }
+                "つす" -> {
+                    --ptr
+//                    println("$ptr M:$cur")
+                }
                 "つう" -> heap[ptr]++
                 "すつ" -> heap[ptr]--
-                "すす" -> print(heap[ptr].toByte().toChar())
+                "すす" -> print(heap[ptr].toInt().toChar())
                 "すう" -> heap[ptr] = readLine()?.get(0)?.toByte()?.toUByte() ?: 0u
                 "うつ" -> if(heap[ptr] == 0u.toUByte()) { mode = Mode.JUMP_F; continue@a }
-                "うす" -> { mode = Mode.JUMP_B; continue@a }
-                "うう" -> print(heap[ptr].toByte())
-
+                "うす" -> if(heap[ptr] != 0u.toUByte()) { mode = Mode.JUMP_B; continue@a }
+                "うう" -> print(heap[ptr])
                 "まて" -> Thread.sleep(heap[ptr].toLong() * 100)
             }
         }
 
         ++cur
+    } } catch (e: Throwable) {
+
+        throw Exception("cur: $cur", e)
+
     }
 
 }
